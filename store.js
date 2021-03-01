9c14bcfd70c9053729f5815afc46ba52cf3b1f88
Amy Lei <42757189+amy-lei@users.noreply.github.com>
2021-03-01 09:37:31 -0500

    Make Store UI-less (#11)
    * Remove UI-handling logic from Store
    
    * Add jsonify/hashing/unhashing methods to Store

--- store.js
+++ store.js
@@ -4,24 +4,14 @@ import { STEP } from "./constants.js";
 
 class Store {
 
-  constructor() {
+  constructor(points) {
     // list of points to be drawn as Point objects
-    this.points = [];
+    this.points = points || [];
     this.addedPoints = [];
     this.violatingPoints = [];
-
-    this.checkResult = "";
-
-    if (document.location.hash && document.location.hash[0] === '#') {
-      this.points = document.location.hash.slice(1).split(';').map(p => {
-        const m = p.trim().match(/\((-?[\d]+),\s*(-?[\d]+)\)/);
-        return m ? new Point(parseInt(m[1]), parseInt(m[2])) : null;
-      }).filter(p => p);
-      this.computeCheck();
-    }
   }
 
-  togglePoint(point) {
+  togglePoint(point, type) {
     const isPoint = (p) => p.equals(point);
     
     const updateSource = (source) => {
@@ -35,7 +25,7 @@ class Store {
         source.push(new Point(point.x, point.y));
     };
 
-    if (document.getElementById("add-grid-point").checked) {
+    if (type === "GRID") {
       // clear added points when we add a new grid point
       this.addedPoints = [];
       updateSource(this.points);
@@ -49,8 +39,6 @@ class Store {
     
     // clear violating points on any add
     this.violatingPoints = [];
-
-    document.location.hash = this.points.map(p => `(${p.x},${p.y})`).join(';');
 
     // check violations interactively
     this.computeCheck();
@@ -67,41 +55,40 @@ class Store {
   computeCheck() {
     let all_points = this.points.concat(this.addedPoints);
     this.violatingPoints = getViolatingPoints(all_points);
-    let notif_string;
-    if (this.violatingPoints.length === 0) {
-      notif_string = "satisfied!";
-    } else {
-      let string_arr = this.violatingPoints.map(a =>
-                       "(" + a[0].x + ", " + a[0].y + ") | (" + a[1].x + ", " + a[1].y + ")");
-      notif_string = "following pairs of points are violating: <br>" +
-                      string_arr.join(" <br>");
-    }
-    this.checkResult = notif_string;
+    return this.violatingPoints;
   }
 
   clearPoints() {
     this.points = [];
     this.addedPoints = [];
     this.violatingPoints = [];
-    this.checkResult = "";
-    document.location.hash = "";
   }
 
-  savePoints(filename){
-    var dummyLink = document.createElement('a');
-    dummyLink.setAttribute('href', 'data:application/json,' + encodeURIComponent(JSON.stringify(this.points)));
-    dummyLink.setAttribute('download', filename);
-    console.log(dummyLink)
-    if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        dummyLink.dispatchEvent(event);
-    }
-    else {
-        dummyLink.click();
-    }
+  toJsonString() {
+    return JSON.stringify(this.store.points);
   }
 
+  /**
+   * Returns hash representation of list of points
+   */
+  hash() {
+    return this.points.map(p => `(${p.x},${p.y})`).join(';');
+  }
+
+  /**
+   * Parse hashed list of points
+   *
+   * @param {String} hashedPoints list of points formatted using `Store.hash`
+   */
+  static unhash(hashedPoints) {
+    return hashedPoints
+      .split(';')
+      .map((p) => {
+        const m = p.trim().match(/\((-?[\d]+),\s*(-?[\d]+)\)/);
+        return m ? new Point(parseInt(m[1]), parseInt(m[2])) : null;
+      })
+      .filter(p => p);
+  }
 }
 
 export { Store };
